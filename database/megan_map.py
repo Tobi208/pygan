078f86e6fe26644067dc02e6344a49c2929f9036
tobi208 <tobias96.home@googlemail.com>
2021-03-10 16:59:54 +0100

    megan_map now fully functional. Made it static instead of oop and created some tests

--- database/megan_map.py
+++ database/megan_map.py
@@ -1,61 +1,59 @@
 import sqlite3
-from typing import List
+from typing import Iterable, Dict
 
 
-class MeganMap:
+def get_accessions2taxonids(database_path: str, accessions: Iterable[str]) -> Dict[str, int]:
     """
-    Interface between megan map database and the program
+    Connect to megan_map.db and create a dictionary of accessions to taxonomy ids
+
+    :param database_path: path of megan_map.db
+    :param accessions: collection of accessions to be mapped
+    :return: dictionary of accessions to taxonomy ids
     """
+    connection = connect(database_path)
+    cursor = connection.cursor()
+    accessions2taxonids = map_accessions(cursor, accessions)
+    disconnect(connection)
+    return accessions2taxonids
 
-    def __init__(self):
-        self.connection = None
-        self.cursor = None
-        self.accension2taxonomy = {}
 
-    def connect(self, db_file: str):
-        """
-        Connect megan map to database file
+def connect(database_path: str) -> sqlite3.Connection:
+    """
+    Connect to megan_map.db
 
-        :param db_file: database file containing megan map
-        """
-        self.connection = sqlite3.connect(db_file)
-        self.cursor = self.connection.cursor()
+    :param database_path: path of megan_map.db
+    :return: sqlite3 connection to megan_map.db
+    """
+    return sqlite3.connect(database_path)
 
-    def load_accession(self, accession: str) -> None:
-        """
-        Select taxonomy from database by accession and add it to dictionary
 
-        :param accession: accession mapped to taxonomy
-        """
-        self.cursor.execute(f'select Taxonomy from mappings where Accession=\'{accession}\'')
-        self.accension2taxonomy[accession] = self.cursor.fetchone()[0]
+def map_accessions(cursor: sqlite3.Cursor, accessions: Iterable[str]) -> Dict[str, int]:
+    """
+    Create a dictionary of accessions to taxonomy ids
 
-    def load_accessions(self, accessions: List[str]) -> None:
-        """
-        Select taxonomies from database by accessions and add them to dictionary
+    :param cursor: sqlite3 cursor to megan_map.db
+    :param accessions: collection of accessions to be mapped
+    :return: dictionary of accessions to taxonomy ids
+    """
+    return {accession: inquire(cursor, accession) for accession in accessions}
 
-        :param accessions: list of accessions mapped to taxonomies
-        :return:
-        """
-        for accession in accessions:
-            self.load_accession(accession)
 
-    def get_taxonomy(self, accension: str) -> str:
-        """
-        Retrieve taxonomy by accession from dictionary or load from database
+def inquire(cursor: sqlite3.Cursor, accession: str) -> int:
+    """
+    Send a query to megan_map.db
 
-        :param accension: accession mapped to taxonomy
-        :return: taxonomy mapped to accession
-        """
-        if accension not in self.accension2taxonomy:
-            self.load_accession(accension)
-        return self.accension2taxonomy[accension]
+    :param cursor: sqlite3 cursor to megan_map.db
+    :param accession: accession to be queried
+    :return: corresponding taxonomy id
+    """
+    cursor.execute(f'select Taxonomy from mappings where Accession=\'{accession}\'')
+    return cursor.fetchone()[0]
 
-    def get_taxonids(self, accensions: List[str]) -> List[str]:
-        """
-        Retrieve taxonomies by accessions from dictionary or load from database
 
-        :param accensions: accessions mapped to taxonomies
-        :return: taxonomies mapped to accessions
-        """
-        return list(map(self.get_taxonomy, accensions))
+def disconnect(connection: sqlite3.Connection):
+    """
+    Disconnect from megan_map.db
+
+    :param connection: sqlite3 connection to megan_map.db
+    """
+    connection.close()
---
+++ tests/database/megan_map_test.py
@@ -0,0 +1,42 @@
+import unittest
+import sqlite3
+
+import database.megan_map as m
+
+
+connection = sqlite3.connect('../../megan-map-Jan2021.db')
+cursor = connection.cursor()
+cursor.execute('select Accession from mappings limit 2000')
+accessions = [x[0] for x in cursor.fetchall()]
+accessions2taxonids = {}
+for accession in accessions:
+    cursor.execute(f'select Taxonomy from mappings where Accession=\'{accession}\'')
+    accessions2taxonids[accession] = cursor.fetchone()[0]
+
+
+class MeganMapTests(unittest.TestCase):
+
+    def test_inquire(self):
+        con = sqlite3.connect('../../megan-map-Jan2021.db')
+        cur = con.cursor()
+        for acc, taxid in accessions2taxonids.items():
+            self.assertEqual(m.inquire(cur, acc), taxid)
+        con.close()
+
+    def test_map_accessions(self):
+        con = sqlite3.connect('../../megan-map-Jan2021.db')
+        cur = con.cursor()
+        accs = accessions2taxonids.keys()
+        self.assertDictEqual(m.map_accessions(cur, accs), accessions2taxonids)
+        con.close()
+
+    def test_get_accessions2taxonids(self):
+        con = sqlite3.connect('../../megan-map-Jan2021.db')
+        cur = con.cursor()
+        accs = accessions2taxonids.keys()
+        self.assertDictEqual(m.get_accessions2taxonids('../../megan-map-Jan2021.db', accs), accessions2taxonids)
+        con.close()
+
+
+if __name__ == '__main__':
+    unittest.main()
