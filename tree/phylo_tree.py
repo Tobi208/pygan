87e7d6fc9ef2e35e8d3bd3bc99ef35581de4778c
tobi208 <tobias96.home@googlemail.com>
2021-04-03 13:57:23 +0200

    added rank and score fields to phylo node

--- tree/phylo_tree.py
+++ tree/phylo_tree.py
@@ -14,11 +14,14 @@ class PhyloNode:
     """
     Primitive phylogentic tree node
 
-    Contains only a tax_id, name, pointer to its parent and list of children
+    Contains only a tax_id, name, rank, pointer to its parent and list of children.
+    Reads indicates the number of reads mapped to this node.
     """
 
     def __init__(self):
         self.tax_id = None
         self.name = None
+        self.rank = None
+        self.reads = None
         self.parent = None
         self.children = []
