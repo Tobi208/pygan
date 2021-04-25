fd366829abdac8a19cfca6f34734b1e5963a35aa
tobi208 <tobias96.home@googlemail.com>
2021-04-25 19:15:54 +0200

    runner script that imports pygan as a module

---
+++ script.py
@@ -0,0 +1,10 @@
+from pygan import lca_analysis
+
+lca_analysis.execute(tre_file='resources/ncbi.tre',
+                     map_file='resources/ncbi.map',
+                     megan_map_file='resources/megan-map-Jan2021.db',
+                     blast_file='resources/Alice01-1mio-Jan-2021.txt',
+                     blast_format='tab', top_score_percent=0.1,
+                     db_segment_size=50000, db_key='Taxonomy',
+                     ignore_ancestors=False, min_support=100,
+                     out_file='lca_analysis.txt')
