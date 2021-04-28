438cc9ac83d5be85f7c5482d5ad1d7f48409a848
tobi208 <tobias96.home@googlemail.com>
2021-04-28 23:48:52 +0200

    10 000 seems to work best for segment size for now

--- script.py
+++ script.py
@@ -5,6 +5,6 @@ lca_analysis.execute(tre_file='resources/ncbi.tre',
                      megan_map_file='resources/megan-map-Jan2021.db',
                      blast_file='resources/Alice01-1mio-Jan-2021.txt',
                      blast_format='tab', top_score_percent=0.1,
-                     db_segment_size=50000, db_key='Taxonomy',
+                     db_segment_size=10000, db_key='Taxonomy',
                      ignore_ancestors=False, min_support=100,
                      out_file='lca_analysis.txt')
